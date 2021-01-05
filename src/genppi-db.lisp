bb5a83256a9602465fc5ea5ad7f7900c6dca2d91
santosardr <santosardr@ufu.br>
2021-01-04 21:13:26 -0300

    Note about the source code

---
+++ src/genppi-db.lisp
@@ -0,0 +1,7 @@
+The GENPPI's source code will be made available as soon as its article got 
+published. We hope it does not take too many months. After that, all you need 
+will be a 'git pull' to access the Common Lisp source code. 
+The binaries were compiled using SBCL 'SBCL 2.0.6.90-a5c377b55'.
+
+Regards,
+Anderson Santos
