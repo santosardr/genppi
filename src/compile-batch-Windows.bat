728501d20e7b52ae9dac1121eac790a0f5c0d0d6
santosardr <santosardr@ufu.br>
2024-01-10 10:44:31 -0300

    Creating all executables

---
+++ src/compile-batch-Windows.bat
@@ -0,0 +1,15 @@
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 4096 --control-stack-size 20  --load genppi.lisp --eval "(save-lisp-and-die \"genppi4g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 8192 --control-stack-size 20  --load genppi.lisp --eval "(save-lisp-and-die \"genppi8g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 16384 --control-stack-size 20  --load genppi.lisp --eval "(save-lisp-and-die \"genppi16g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 32768 --control-stack-size 20  --load genppi.lisp --eval "(save-lisp-and-die \"genppi32g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 4096 --control-stack-size 20  --load genppidb.lisp --eval "(save-lisp-and-die \"genppidb4g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 8192 --control-stack-size 20  --load genppidb.lisp --eval "(save-lisp-and-die \"genppidb8g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 16384 --control-stack-size 20  --load genppidb.lisp --eval "(save-lisp-and-die \"genppidb16g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
+
+"C:\Program Files\Steel Bank Common Lisp\sbcl.exe" --noinform --dynamic-space-size 32768 --control-stack-size 20  --load genppidb.lisp --eval "(save-lisp-and-die \"genppidb32g-Win.exe\" :executable t :save-runtime-options t :toplevel 'main)"
